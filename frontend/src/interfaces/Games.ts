0ce40d1a86b7236d6c4f183f5dca14cff5532716
mateusfelixdias <felixmateus618@gmail.com>
2022-11-16 11:44:00 -0300

    feat: added interfaces

---
+++ frontend/src/interfaces/Games.ts
@@ -0,0 +1,6 @@
+export interface IGames {
+  bannerUrl: string
+  id: string
+  title: string
+  _count: { ads: number }
+}
---
+++ frontend/src/interfaces/PropsGameBanner.ts
@@ -0,0 +1,5 @@
+export interface IPropsGameBanner {
+  bannerUrl: string
+  title: string
+  adsCount: number
+}
