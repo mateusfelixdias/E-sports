07a137590c12a86217eca3efac7fff2a02f7509b
mateusfelixdias <felixmateus618@gmail.com>
2022-11-16 11:53:28 -0300

    feat: added Carrosel

---
+++ frontend/src/components/Carrossel/Carrossel.tsx
@@ -0,0 +1,40 @@
+import { api } from '../../lib/api'
+import { useEffect, useState } from 'react'
+import { IGames } from '../../interfaces/Games'
+import AliceCarousel from 'react-alice-carousel'
+import { GameBanner } from '../Banner/GameBanner'
+import { bannerCarouselPesponsiveness } from '../../utils/bannerCarouselPesponsiveness'
+
+import 'react-alice-carousel/lib/alice-carousel.css'
+
+export function Carrossel() {
+  const [games, setGames] = useState<Array<IGames>>([])
+
+  useEffect(() => {
+    const games = async () => {
+      const { data }: any = await api.get('/games')
+
+      const games = data.map(({ bannerUrl, _count: { ads }, id, title }: any) => {
+        return <GameBanner adsCount={ads} bannerUrl={bannerUrl} key={id} title={title} />
+      })
+
+      setGames(games)
+    }
+
+    games()
+  }, [])
+
+  return (
+    <div className="flex w-[80%] gap-6 mt-16">
+      <AliceCarousel
+        mouseTracking
+        items={games}
+        paddingRight={20}
+        disableDotsControls
+        touchMoveDefaultEvents
+        controlsStrategy="alternate"
+        responsive={bannerCarouselPesponsiveness}
+      />
+    </div>
+  )
+}
