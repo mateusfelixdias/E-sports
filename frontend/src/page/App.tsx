29d9ccd8f9c245a248e42b08dd5df70bc70d6023
mateusfelixdias <felixmateus618@gmail.com>
2022-11-16 11:47:18 -0300

    feat: added app

---
+++ frontend/src/page/App.tsx
@@ -0,0 +1,24 @@
+import * as Dialog from '@radix-ui/react-dialog'
+import logoImg from '../assets/logo-nlw-esports.svg'
+import { Carrossel } from '../components/Carrossel/Carrossel'
+import { CreateAdModal } from '../components/Modal/CreateAdModal'
+import { CreateAdBaneer } from '../components/Banner/CreateAdBaneer'
+
+export function App() {
+  return (
+    <div className="w-full mx-auto flex flex-col items-center my-10">
+      <img src={logoImg} alt="" />
+      <h1 className="text-6xl text-white font-black mt-20 target:w-full table:text-4xl">
+        Seu <span className="text-transparent bg-nlw-gradient bg-clip-text">duo</span> está aqui!
+      </h1>
+
+      <Carrossel />
+
+      <Dialog.Root>
+        <CreateAdBaneer />
+
+        <CreateAdModal />
+      </Dialog.Root>
+    </div>
+  )
+}
