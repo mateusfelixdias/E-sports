292ca102c1a980ee4e70cf539175e61b25e14ef2
mateusfelixdias <felixmateus618@gmail.com>
2022-11-17 20:03:35 -0300

    feat: placed path to image

--- frontend/src/components/Banner/GameBanner.tsx
+++ frontend/src/components/Banner/GameBanner.tsx
@@ -5,7 +5,7 @@ export function GameBanner({ adsCount, bannerUrl, title }: IPropsGameBanner) {
 
   return (
     <>
-      <a href="" className="relative rounded-lg overflow-hidden">
+      <a href={bannerUrl} className="relative rounded-lg overflow-hidden">
         <img src={bannerUrl} alt="" />
 
         <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 right-0 left-0">
