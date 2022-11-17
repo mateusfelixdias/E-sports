945be2d4a0448301b79bd24236bed24817b37b8f
mateusfelixdias <felixmateus618@gmail.com>
2022-11-17 20:21:43 -0300

    feat: added list With Days Of The Week

---
+++ frontend/src/utils/toggleGroup/listWithDaysOfTheWeek.ts
@@ -0,0 +1,37 @@
+export const listWithDaysOfTheWeek = [
+  {
+    title: 'Domingo',
+    value: '0',
+    firstLetterOfTheDay: 'D',
+  },
+  {
+    title: 'Segunda',
+    value: '1',
+    firstLetterOfTheDay: 'S',
+  },
+  {
+    title: 'Terça',
+    value: '2',
+    firstLetterOfTheDay: 'T',
+  },
+  {
+    title: 'Quarta',
+    value: '3',
+    firstLetterOfTheDay: 'Q',
+  },
+  {
+    title: 'Quinta',
+    value: '4',
+    firstLetterOfTheDay: 'Q',
+  },
+  {
+    title: 'Sexta',
+    value: '5',
+    firstLetterOfTheDay: 'S',
+  },
+  {
+    title: 'Sábado',
+    value: '6',
+    firstLetterOfTheDay: 'S',
+  },
+]
