8179df6d7e55907b908af39833e81fb5a1c90a93
mateusfelixdias <felixmateus618@gmail.com>
2022-11-17 20:32:58 -0300

    feat: added component select

---
+++ frontend/src/components/Form/Select.tsx
@@ -0,0 +1,53 @@
+import { api } from '../../lib/api'
+import { useEffect, useState } from 'react'
+import * as Select from '@radix-ui/react-select'
+import { ChevronDownIcon } from '@radix-ui/react-icons'
+import { IPropsGameSelect } from '../../interfaces/PropsGameSelect'
+
+export function SelectGames() {
+  const [games, setGames] = useState<Array<IPropsGameSelect>>([])
+
+  useEffect(() => {
+    const games = async () => {
+      const { data }: any = await api.get('/games')
+
+      setGames(data)
+    }
+
+    games()
+  }, [])
+
+  return (
+    <>
+      <Select.Trigger
+        className="bg-zinc-900 py-3 px-4 rounded text-sm inline-flex items-center justify-between"
+        aria-label="Food"
+      >
+        <Select.Value placeholder="Selecione o gamer que deseja jogar" />
+        <Select.Icon className="bg-violet-400 hover:bg-violet-700 rounded">
+          <ChevronDownIcon />
+        </Select.Icon>
+      </Select.Trigger>
+
+      <Select.Portal>
+        <Select.Content className="bg-zinc-900 rounded absolute px-4 pb-2 w-full">
+          <Select.Viewport>
+            <Select.Group>
+              {games.map(({ id, title }) => {
+                return (
+                  <Select.Item
+                    key={id}
+                    value={id}
+                    className="text-1xl flex items-center relative text-zinc-300"
+                  >
+                    <Select.ItemText>{title}</Select.ItemText>
+                  </Select.Item>
+                )
+              })}
+            </Select.Group>
+          </Select.Viewport>
+        </Select.Content>
+      </Select.Portal>
+    </>
+  )
+}
