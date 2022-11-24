b4dda1563c045afed3588205d2f5c7c4b233e6c5
mateusfelixdias <felixmateus618@gmail.com>
2022-11-24 13:15:39 -0300

    refactor: create ad modal

--- frontend/src/components/Modal/CreateAdModal.tsx
+++ frontend/src/components/Modal/CreateAdModal.tsx
@@ -1,65 +1,96 @@
-import * as Yup from 'yup'
 import classNames from 'classnames'
 import { Input } from '../Form/Input'
-import { FormEvent, useState } from 'react'
+import { Error } from '../Form/Alerts/Error'
 import { SelectGames } from '../Form/Select'
+import { IForm } from '../../interfaces/form'
+import { Sucess } from '../Form/Alerts/Sucess'
 import * as Select from '@radix-ui/react-select'
 import * as Dialog from '@radix-ui/react-dialog'
 import * as Checkbox from '@radix-ui/react-checkbox'
+import { FormEvent, useEffect, useState } from 'react'
 import { Check, GameController } from 'phosphor-react'
 import * as ToggleGroup from '@radix-ui/react-toggle-group'
 import { createAd } from '../../utils/createAdModal/createAd'
+import { emptyForm } from '../../utils/createAdModal/emptyForm'
 import { daysOfTheWeekSelected } from '../../utils/toggleGroup/daysOfTheWeekSelected'
 import { listWithDaysOfTheWeek } from '../../utils/toggleGroup/listWithDaysOfTheWeek'
 
 export function CreateAdModal() {
-  const [weekDays, setWeekDays] = useState<string[]>([])
-  const [useVoiceChannel, setUseVoiceChannel] = useState<boolean>(false)
-  const [playerSelectedGame, setPlayerSelectedGame] = useState<string>('')
-  const [loadingAdCreation, setLoadingAdCreation] = useState<boolean>(false)
-  const [fieldsAreValid, setFieldsAreValid] = useState<boolean | Yup.ValidationError[]>(false)
+  const [creatingAd, setCreatingAd] = useState<boolean>(false)
+  const [errorMessage, setErrorMessage] = useState<string>('')
+  const [successMessage, setSuccessMessage] = useState<boolean>(false)
+  const [theFieldsAreInvalid, setTheFieldsAreInvalid] = useState<boolean>(false)
+  const [form, setForm] = useState<IForm>(emptyForm)
 
-  function onCheckedChange(checked: Checkbox.CheckedState) {
+  const handleCreateAd = async (event: FormEvent) => {
+    event.preventDefault()
+    setCreatingAd(true)
+
+    const response = await createAd(form)
+
+    if (typeof response === 'string') return handleError(response)
+
+    return handleSucess()
+  }
+
+  const handleError = (response: string) => {
+    setCreatingAd(false)
+    setErrorMessage(response)
+    return
+  }
+
+  const handleSucess = () => {
+    setTimeout(() => {
+      setSuccessMessage(true)
+      setCreatingAd(false)
+      setForm(emptyForm);
+
+      setTimeout(() => setSuccessMessage(false), 5000)
+    }, 1000)
+  }
+
+  const onCheckedChange = (checked: Checkbox.CheckedState) => {
     if (checked === true) {
-      setUseVoiceChannel(true)
+      setForm({ ...form, useVoiceChannel: true })
       return
     }
 
-    setUseVoiceChannel(false)
+    setForm({ ...form, useVoiceChannel: false })
   }
 
-  async function handleCreateAd(event: FormEvent) {
-    event.preventDefault()
+  useEffect(() => {
+    if (errorMessage.length) setTheFieldsAreInvalid(true)
 
-    const formData = new FormData(event.target as HTMLFormElement)
-    const { discord, hourEnd, hourStart, name, yearsPlaying } = Object.fromEntries(formData)
-
-    await createAd(
-      discord,
-      hourEnd,
-      hourStart,
-      name,
-      yearsPlaying,
-      useVoiceChannel,
-      playerSelectedGame,
-      weekDays
-    )
-  }
+    setTimeout(() => {
+      setTheFieldsAreInvalid(false)
+      setErrorMessage('')
+    }, 5000)
+  }, [errorMessage])
 
   return (
     <>
       <Dialog.Portal>
         <Dialog.Overlay className="bg-black/60 inset-0 fixed" />
         <Dialog.Content className="fixed bg-[#2A2634] text-white rounded-lg py-8 px-10 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 table:w-full">
+          {theFieldsAreInvalid ? (
+            <Error message={errorMessage} />
+          ) : successMessage ? (
+            <Sucess />
+          ) : null}
+
           <Dialog.Title className="text-3xl font-black">Públique um anúncio</Dialog.Title>
 
-          <form onSubmit={handleCreateAd} className="mt-8 flex flex-col gap-4">
+          <form className="mt-8 flex flex-col gap-4">
             <div className="flex flex-col gap-2">
               <label htmlFor="game" className="font-semibold">
                 Qual seu game?
               </label>
 
-              <Select.Root onValueChange={setPlayerSelectedGame} required>
+              <Select.Root
+                name="game"
+                onValueChange={(value: string) => setForm({ ...form, playerSelectedGame: value })}
+                required
+              >
                 <SelectGames />
               </Select.Root>
             </div>
@@ -67,7 +98,13 @@ export function CreateAdModal() {
             <div className="flex flex-col gap-2">
               <label htmlFor="name">Seu nome (ou nickname)</label>
 
-              <Input name="name" id="name" placeholder="como te chamam detro do game?" required />
+              <Input
+                name="name"
+                required
+                placeholder="como te chamam detro do game?"
+                value={form.name}
+                onChange={({ target: { value } }) => setForm({ ...form, name: value })}
+              />
             </div>
 
             <div className="grid grid-cols-2 gap-6">
@@ -77,19 +114,25 @@ export function CreateAdModal() {
                 <Input
                   required
                   type="number"
-                  id="yearsPlaying"
                   name="yearsPlaying"
                   placeholder="Tudo bem ser ZERO"
+                  value={form.yearsPlaying}
+                  onChange={({ target: { value } }) => setForm({ ...form, yearsPlaying: value })}
                 />
               </div>
 
               <div className="flex flex-col gap-2">
                 <label htmlFor="discord">Qual seu discord</label>
 
-                <Input name="discord" id="discord" placeholder="usuario#0000" required />
+                <Input
+                  name="discord"
+                  required
+                  placeholder="usuario#0000"
+                  value={form.discord}
+                  onChange={({ target: { value } }) => setForm({ ...form, discord: value })}
+                />
               </div>
             </div>
-
             <div className="flex gap-6">
               <div className="flex flex-col gap-2">
                 <label htmlFor="weekdays">Quando costuma jogar?</label>
@@ -97,16 +140,16 @@ export function CreateAdModal() {
                 <ToggleGroup.Root
                   type="multiple"
                   className="grid grid-cols-4 gap-2"
-                  value={weekDays}
-                  onValueChange={setWeekDays}
+                  value={form.weekDays}
+                  onValueChange={(value: string[]) => setForm({ ...form, weekDays: value })}
                 >
                   {listWithDaysOfTheWeek.map(({ firstLetterOfTheDay, title, value }) => {
                     return (
                       <ToggleGroup.Item
                         key={title}
                         className={classNames('w-8 h-8 rounded', {
-                          'bg-violet-700': daysOfTheWeekSelected(weekDays, value),
-                          'bg-zinc-900': !daysOfTheWeekSelected(weekDays, value),
+                          'bg-violet-700': daysOfTheWeekSelected(form.weekDays, value),
+                          'bg-zinc-900': !daysOfTheWeekSelected(form.weekDays, value),
                         })}
                         title={title}
                         value={value}
@@ -122,8 +165,23 @@ export function CreateAdModal() {
                 <label htmlFor="hourStart">Qual horário do dia?</label>
 
                 <div className="grid grid-cols-2 gap-2">
-                  <Input required name="hourStart" id="hourStart" type="time" placeholder="De" />
-                  <Input required name="hourEnd" id="hourEnd" type="time" placeholder="Até" />
+                  <Input
+                    required
+                    name="hourStart"
+                    type="time"
+                    placeholder="De"
+                    value={form.hourStart}
+                    onChange={({ target: { value } }) => setForm({ ...form, hourStart: value })}
+                  />
+
+                  <Input
+                    required
+                    name="hourEnd"
+                    type="time"
+                    placeholder="Até"
+                    value={form.hourEnd}
+                    onChange={({ target: { value } }) => setForm({ ...form, hourEnd: value })}
+                  />
                 </div>
               </div>
             </div>
@@ -131,7 +189,7 @@ export function CreateAdModal() {
             <label className="mt-2 flex items-center gap-2 text-sm">
               <Checkbox.Root
                 className="w-6 h-6 p-1 rounded bg-zinc-900"
-                checked={useVoiceChannel}
+                checked={form.useVoiceChannel}
                 onCheckedChange={onCheckedChange}
               >
                 <Checkbox.Indicator>
@@ -140,20 +198,19 @@ export function CreateAdModal() {
               </Checkbox.Root>
               Costumo me conectar ao chat de voz
             </label>
-
             <footer className="mt-4 flex justify-end gap-4">
               <Dialog.Close className="bg-zinc-500 px-5 h-12 rounded-md font-semibold hover:bg-zinc-600">
                 Cancelar
               </Dialog.Close>
 
               <button
-                className="bg-violet-500 px-5 h-12 rounded-md font-semibold flex items-center gap-3 hover:bg-violet-600"
-                disabled={loadingAdCreation}
-                type="submit"
-                onSubmit={() => setLoadingAdCreation(true)}
+                className="bg-violet-500 px-5 h-12 rounded-md font-semibold flex items-center gap-3 hover:bg-violet-700 disabled:bg-violet-900"
+                disabled={theFieldsAreInvalid || creatingAd}
+                type="button"
+                onClick={handleCreateAd}
               >
                 <GameController size={24} />
-                {loadingAdCreation ? 'Carregando...' : 'Cadastrar'}
+                {creatingAd ? 'Carregando...' : 'Cadastrar'}
               </button>
             </footer>
           </form>
