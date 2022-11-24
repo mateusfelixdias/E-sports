369fc17fc2acb8c75071ba957ae58d0aca9c5971
mateusfelixdias <felixmateus618@gmail.com>
2022-11-24 12:01:15 -0300

    feat: added empty form

---
+++ frontend/src/utils/createAdModal/emptyForm.ts
@@ -0,0 +1,12 @@
+import { IForm } from "../../interfaces/form";
+
+export const emptyForm: IForm = {
+  discord: '',
+  hourStart: '',
+  hourEnd: '',
+  name: '',
+  weekDays: [],
+  yearsPlaying: '',
+  playerSelectedGame: '',
+  useVoiceChannel: false,
+}
